744fc9c13e3a1dd6ee6ac217bb73b278ea2cbcfa
SocolaDaiCa <tokentien@gmail.com>
2018-02-13 10:37:06 +0700

    coding

--- db/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('hostName', 'localhost');
-	define('username', 'tentstud_bot');
-	define('password', 'xC50BIN6L9Bt');
-	define('database', 'tentstud_bot');
-?>
--- test/test-timezone.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$time = $_GET['time'];
-	date_default_timezone_set('UTC');
-	$i =2;
-		$time = date("d/m/Y H:i:s", strtotime("{$time} hour"));
-	// $time = date("d/m/Y H:i:s");
-	file_put_contents('time.txt', $time);
-
-?>
